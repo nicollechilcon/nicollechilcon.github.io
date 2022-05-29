55898432ad43293bcf9d64687ec013641cea53ee
fiorellachilcon <fiorellachilc_20@hotmail.com>
2022-05-28 21:28:16 -0500

    chore: update font

--- src/components/about-me-hero/index.jsx
+++ src/components/about-me-hero/index.jsx
@@ -1,6 +1,7 @@
+import me from '../../../src/assets/images/me.png';
+import { Link } from 'react-router-dom';
 import React from 'react';
 import './index.css';
-import me from '../../../src/assets/images/me.png';
 
 export function AboutMeHero() {
   return (
@@ -10,7 +11,8 @@ export function AboutMeHero() {
         <h2>UX Designer</h2>
         <div>
           I have a passion for expressing my creativity through the design of digital platforms.
-          Being a UX Designer, I seek to create the best experiences for users using my infinite curiosity and caffeine. More <strong>about me</strong> 👋
+          Being a UX Designer, I seek to create the best experiences for users using my infinite curiosity and caffeine.
+          More <Link to="/about-me"><strong>about me</strong></Link> 👋
         </div>
       </section>
       <img alt="" src={me}/>
