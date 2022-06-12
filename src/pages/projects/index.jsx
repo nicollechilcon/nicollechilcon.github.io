5a10549c019b2dd59b7e7be8c4e83ca9d5666d72
fiorellachilcon <fiorellachilc_20@hotmail.com>
2022-06-11 20:34:07 -0500

    feat: scroll to top button

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -7,14 +7,36 @@ import { ProjectProblems } from '../../components/project-problems';
 import { ProjectPrototypes } from '../../components/project-prototypes';
 import { ProjectLink } from '../../components/project-link';
 import { ProjectHero } from '../../components/project-hero';
+import topArrow from '../../assets/images/top-arrow.svg';
 import { projects } from '../../data/projects';
 import { useParams } from 'react-router-dom';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './index.css';
 
 export function Projects() {
   const { id } = useParams();
+  const [showScrollToTopBtn, setShowScrollToTopBtn] = useState(false);
   const project = projects.find(project => id === project.id);
   const [nextProject] = projects.filter(project => id !== project.id).sort(() => 0.5 - Math.random());
+
+  const handleScrollBackToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const handleScrollState = () =>  {
+    const normalDistance = document.body.offsetHeight / 3;
+    const scroll = window.scrollY + window.innerHeight;
+    setShowScrollToTopBtn(scroll >= normalDistance);
+  };
+
+  useEffect(() => {
+    handleScrollState();
+    window.addEventListener('scroll', handleScrollState);
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollState);
+    };
+  }, []);
 
   return (
     <>
@@ -29,6 +51,11 @@ export function Projects() {
           <ProjectPrototypes content={project.content.prototype} />
           <ProjectConclusion content={project.content.conclusion} />
           {nextProject && <ProjectLink id={nextProject.id} content={nextProject.link} />}
+          {showScrollToTopBtn && (
+            <button className="scroll-top-btn" onClick={handleScrollBackToTop} title="Scroll back to top">
+              <img src={topArrow} alt="Scroll back to top" />
+            </button>
+          )}
         </div>
       }
     </>
